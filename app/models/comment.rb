406d901bccc23bb4d542caba60d0d1f9f358fcb6
Dan Nollette <nollette.dan@gmail.com>
2017-12-07 16:23:49 -0800

    Remove old comment add replace relationship between user and comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,7 @@
 class Comment < ApplicationRecord
-  # belongs_to :user
-  # temporarily cut out user so comment creation works, pending addition of authorization
+  belongs_to :user
   belongs_to :product
   validates :text_body, :presence => true
-  # validates :user_id, :presence => true
+  validates :user_id, :presence => true
   validates :product_id, :presence => true
 end
